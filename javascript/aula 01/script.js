b99eae3d91c71897da40aa8d9f77fbc85a5df820
giovitoriaa <olivergiovanna57@gmail.com>
2025-01-31 12:24:55 -0300

    adicionando js

---
+++ javascript/aula 01/script.js
@@ -0,0 +1,19 @@
+console.log("olá mundo!");
+
+//exemplo de alert()
+alert("olá mundo 1")
+//exemplo de confirm()
+var teste = confirm ("Ola mundo 2")
+//exibindo a opção escolhida pelo usuário
+console.log(teste)
+
+//exemplo de prompt()
+    var testePrompt = prompt ("Digite seu nome")
+    //exibindo o nome do usuario 
+    console.log(testePrompt)
+//tentativa de adição
+    var numero1 = Number.parseInt(prompt("Digite o primeiro numero"))
+    var numero2 = Number.parseInt(prompt("Digite o segundo numero"))
+    //verificando o tipo de variavel 
+    console.log(typeof(numero1))
+    alert(numero1 + numero2)
